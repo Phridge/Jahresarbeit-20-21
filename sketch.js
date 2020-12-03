4b1a8805e01819432aba359af9ddfa5f1d933fd3
Vincent Eichhorn <v.hase03@gmx.de>
2020-12-03 19:20:06 +0100

    init canvas, animation, draw with plain html 5 canvas, context

--- sketch.js
+++ sketch.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+(() => {
+
+    const height = 300;
+    const widht = 500;
+
+    let x = 0;
+    let speed = 3;
+
+    function animate() {
+        requestAnimationFrame(animate);
+        x += speed;
+        if(x >= 500) x = 0;
+        draw();
+    }
+
+    function draw() {
+        let canvas = document.getElementById('canvas');
+        let ctx = canvas.getContext('2d');
+
+        ctx.clearRect(0, 0, 500, 300);
+
+        ctx.fillStyle = '#dfac20';
+        ctx.fillRect(x, 10, 40, 40);
+    }
+
+    document.addEventListener("DOMContentLoaded", () => {
+        animate();
+    });
+
+})();
