039b557c51921b6e73a587435636689f1a5e2650
Erdwig <rl.foerster.2002@gmail.com>
2021-01-13 11:54:37 +0100

    First attempt at Javascript tutorial

--- code_begleitung_tutorial/03_js_basic/js/sketch.js
+++ code_begleitung_tutorial/03_js_basic/js/sketch.js
@@ -1,5 +1,60 @@
-console.log("Hello World");
+let someString = "Evolution!";
 
-function helloWorld() {
-    alert("Hello World");
+// let someString = "Was anderes!" // nicht machen
+
+console.log(someString);
+
+window.alert(someString);
+
+function printRepeat(value, times) {
+    let i = 0;
+
+    while(i < times) {
+        console.log(value);
+        i = i + 1;
+    }
 }
+
+printRepeat(someString, 5)
+
+printRepeat = function(value, times) {
+    for (let i = 0; i < times; i++) {
+        console.log(value);
+    } 
+};
+
+printRepeat(someString, 5);
+
+let doRepeat = (func, times) => {
+    for(let i = 0; i < times; i++) {
+        func();
+    }
+};
+
+doRepeat(
+    () => alert("Hello World!"),
+    3
+);
+
+function add(a, b) {
+    return a + b
+}
+
+console.log(add("123", "456"))
+console.log(add(123, 456))
+console.log(add("123", 456))
+console.log(add(123, "456"))
+
+function min(a, b) {
+    if(a < b) {
+        return a
+    } else {
+        return b
+    }
+}
+
+let print = console.log
+
+print(min(2, 1))
+print(min(4, 7))
+print(min("Hello", "World"))
