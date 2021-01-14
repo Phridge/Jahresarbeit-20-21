ed3175eb84655c9ffa0d5c51d304b5cae8005b76
Vincent Eichhorn <v.hase03@gmx.de>
2021-01-14 19:52:57 +0100

    3. js part

--- code_begleitung_tutorial/03_js_basic/js/sketch.js
+++ code_begleitung_tutorial/03_js_basic/js/sketch.js
@@ -1,60 +1,48 @@
-let someString = "Evolution!";
+//Erster Code
+let message = "Das ist eine Nachricht!";
+window.alert(message);
+console.log(message);
 
-// let someString = "Was anderes!" // nicht machen
+//Erster Code 2
+let message = "Das ist eine Nachricht!";
+let reply = "Das ist eine Antwort!"
+window.alert(message + reply);
+let numberA = 20;
+let numberB = 22;
+let numberC = numberA + numberB;
+console.log("20 + 22 = " + numberC);
+console.log("Zwanzig plus Zweiundzwanzig ist " + (numberA + numberB));
 
-console.log(someString);
+//Funktionen
+function addNumbers(a, b) {
+    let c = a + b;
+    console.log("Die Antwort ist " + c);
+}
+addNumbers(3, 5);
+addNumbers(10, 14);
 
-window.alert(someString);
+function f(x) {
+    return 5 * x + 3;
+}
+console.log("5 * 5 + 3 = " + f(5));
 
-function printRepeat(value, times) {
-    let i = 0;
-
-    while(i < times) {
-        console.log(value);
-        i = i + 1;
+//Verzweigungen
+function min(a, b) {
+    if(a < b) {
+        return a;
+    } else {
+        return b;
     }
 }
 
-printRepeat(someString, 5)
+console.log(min(2, 1));
+console.log(min(4, 7));
 
-printRepeat = function(value, times) {
+//Schleifen
+function printRepeat(value, times) {
     for (let i = 0; i < times; i++) {
         console.log(value);
     } 
 };
 
-printRepeat(someString, 5);
-
-let doRepeat = (func, times) => {
-    for(let i = 0; i < times; i++) {
-        func();
-    }
-};
-
-doRepeat(
-    () => alert("Hello World!"),
-    3
-);
-
-function add(a, b) {
-    return a + b
-}
-
-console.log(add("123", "456"))
-console.log(add(123, 456))
-console.log(add("123", 456))
-console.log(add(123, "456"))
-
-function min(a, b) {
-    if(a < b) {
-        return a
-    } else {
-        return b
-    }
-}
-
-let print = console.log
-
-print(min(2, 1))
-print(min(4, 7))
-print(min("Hello", "World"))
+printRepeat("Wiederholung", 10);
