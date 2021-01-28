e35c9ee89b43f860c969248fb28fc50e199c811c
Erdwig <rl.foerster.2002@gmail.com>
2021-01-28 19:17:33 +0100

    chapter 21 adding consumers initial commit

---
+++ code_begleitung_tutorial/22_adding_consumers/js/position.js
@@ -0,0 +1,54 @@
+/**
+ * Diese Klasse beschreibt eine Position im 2Dimensionalen Raum.
+ */
+class Position {
+    /**
+     * Kreiere eine Position von x- und y-Koordinate.
+     * @param {*} x - die x-Koordinate
+     * @param {*} y - die y-Koordinate
+     */
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Addiere zwei Positionen.
+     * @param {Position} other - die andere Position
+     * @return die Summe beider Positionen
+     */
+    add(other) {
+        return new Position(this.x + other.x, this.y + other.y);
+    }
+    /**
+     * Subtrahiere zwei Positionen.
+     * @param {Position} other - die andere Position
+     * @return die Differenz beider Positionen
+     */
+    sub(other) {
+        return new Position(this.x - other.x, this.y - other.y);
+    }
+
+    /**
+    * Multipliziere diese Position mit einer Zahl
+    * @param {Number} factor - der Wert mit dem multipliziert wird
+    * @return diese Position mit factor multipliziert
+    */
+    mul(factor) {
+        return new Position(this.x * factor, this.y * factor);
+    }
+
+    /**
+     * Berechnet die Distanz zu einer anderen Position.
+     * Wird berechnet via Pythagoras: a = sqrt(b^2, c^2),
+     * wobei b hier die Distanz zwischen den x-Koordinaten
+     * und c die Distanz zwischen den y-Koordinaten der Positionen ist.
+     * @param {Position} to - die andere Position
+     * @return die distanz zwischen this und der anderen Position
+     */
+    dist(to) {
+        const dx = this.x - to.x;
+        const dy = this.y - to.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
