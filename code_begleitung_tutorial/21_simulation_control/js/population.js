6cc9ab48220d769dcf5487fcbce8b8868e1330dd
Erdwig <rl.foerster.2002@gmail.com>
2021-01-28 19:07:50 +0100

    chapter 21 simulation control initial commit

---
+++ code_begleitung_tutorial/21_simulation_control/js/population.js
@@ -0,0 +1,95 @@
+
+/**
+ * Die "Population"-Klasse stellt eine Sammlung von mehreren
+ * Städten dar, die den Evolutionsverlauf simuliert.
+ * Nachdem eine Population Ausgangs-Städte erhalten hat,
+ * mit denen die Evolution begonnen werden kann, werden immer
+ * wieder diese Schritte wiederholt:
+ * * Selektion
+ * * Repopulation
+ * * Mutation
+ * Diese Schritte sind alle in der Methode "nextPopulation"
+ * wiederzufinden. Jede ausgeführte Evolutionsrunde nennt man
+ * auch eine "Epoche"
+ */
+class Population {
+    /**
+     * Kreiere eine neue Population mit einer Start-Stadt (initialCity)
+     * und bestimmter Konfigurationen, z.B. wie groß die Kapazität der
+     * Population ist.
+     * @param {Object} config - Konfigurationsobjekt
+     * @param {City} initialCity - Stadt, die zu beginn die Kapazitäten füllen wird
+     */
+    constructor(config, initialCity) {
+        this.cities = new Array(config.size); 
+        this.config = config;
+
+        // füllen der Population, wir nutzen einfach repopulate
+        this.repopulate(initialCity);
+    }
+
+    /**
+     * Ermittelt das beste Individuum/Stadt der Population.
+     * @return die Stadt mit der höchsten Fitness
+     */
+    getFittest() {
+         // irgendeine Stadt für den Anfang
+        let bestCity = this.cities[0];
+
+        for(let i = 1; i < this.cities.length; i++) {
+            let city = this.cities[i];
+            if(city.getFitness(this.config) > bestCity.getFitness(this.config)) {
+                bestCity = city;
+            }
+        }
+
+        return bestCity;
+    }
+
+    /**
+     * Simuliert eine Epoche in folgenden Einzelschritten:
+     * * Selektion
+     * * Repopulation
+     * * Mutation
+     */
+    nextGeneration() {
+        // Selektion
+        let best = this.getFittest();
+
+        // config.size ist vielleicht größer/kleiner als vorher
+        let newCities = new Array(this.config.size);
+
+        // Repopulation
+        for (let i = 0; i < newCities.length; i++) {
+            let newCity = best.clone();
+
+            // Mutation
+            newCity.mutate(this.config);
+
+            newCities[i] = newCity;
+        }
+
+        this.cities = newCities;
+    }
+
+    /**
+     * Falls Konfigurationen geändert wurden, kann man so dieser
+     * Population die Änderungen mitteilen.
+     * @param {Object} config - Konfigurationsobjekt, das evtl. verändert wurde
+     */
+    updateConfig(config) {
+        this.config = config;
+    }
+
+    /**
+     * Setze die Population zurück - das bedeutet, alle Kapazitäten
+     * der Population werden mit ein und der selben Stadt gefüllt.
+     * @param {City} initialCity - Stadt, die die Kapazitäten füllen wird
+     */
+    repopulate(initialCity) {
+        this.cities = new Array(this.config.size);
+        for(let i = 0; i < this.cities.length; i++) {
+            this.cities[i] = initialCity;
+        }
+    }
+}
---
+++ code_begleitung_tutorial/21_simulation_control/js/sketch.js
@@ -0,0 +1,161 @@
+function sketch() {
+    let canvas = document.getElementById("canvas");
+
+    let width = 500, height = 300;
+    canvas.width = width;
+    canvas.height = height;
+
+    let ctx = canvas.getContext("2d");
+
+    let initialCity = new City();
+    initialCity.addTransformer(new Position(300, 100)); // Index 0
+    initialCity.addNode(new Position(420, 100), 0); // Index 1
+    initialCity.addNode(new Position(170, 180), 0); // Index 2
+    initialCity.addConsumer(new Position(100, 50), 2); // Index 3
+    initialCity.addConsumer(new Position(400, 200), 1); // Index 4
+    initialCity.addConsumer(new Position(50, 250), 2); // Index 5
+    initialCity.addConsumer(new Position(450, 50), 1); // Index 6
+    initialCity.addConsumer(new Position(250, 200), 2); // Index 7
+
+    // Alle Konfigurationen, die für Population/City interessant sind
+    let populationConfig = {
+        size: 20,
+        maxMoveDelta: 5,
+        moveChance: 3 / 100,
+        nodeMutationChance: 2 / 100,
+        reconnectChance: 3 / 100,
+        nodePenalty: 5
+    };
+
+    // Status/Konfigurationen, die nichts mit Population/City zu tun haben
+    let simulationState = {
+        generationCount: 0,
+        isSimulating: true
+    };
+
+    let population = new Population(populationConfig, initialCity);
+
+    function animate() {
+        ctx.clearRect(0, 0, width, height);
+        
+        // Zeichnen der besten Stadt
+        let best = population.getFittest();
+        best.draw(ctx);
+        console.log(best.getFitness());
+        
+        // Daten in der Sidebar anzeigen
+        document.getElementById('result-length').innerHTML = "Länge: " + Math.round(best.getLength()) + 'm';
+        document.getElementById('result-generations').innerHTML = "Generation: " + simulationState.generationCount;
+        document.getElementById('result-consumers').innerHTML = "Häuser: " + best.cityObjects.filter(obj => obj instanceof Consumer).length;
+        document.getElementById('result-nodes').innerHTML = "Knoten: " + best.cityObjects.filter(obj => obj instanceof Node).length;
+        document.getElementById("result-connections").innerHTML = "Verbindungen: " + (best.cityObjects.length - 1);
+
+        if(simulationState.isSimulating) {
+
+            // Nächste Epoche simulieren
+            population.nextGeneration();
+            
+            // Epoche fertig
+            simulationState.generationCount += 1;
+        }
+        
+        requestAnimationFrame(animate);
+    }
+
+    // Buttons EventListeners
+
+    // Start/Stop
+    document.getElementById('action-simulate').addEventListener('click', event => {
+        if (simulationState.isSimulating) {
+            event.target.innerHTML = "starten";
+            simulationState.isSimulating = false;
+        } else {
+            event.target.innerHTML = "stoppen";
+            simulationState.isSimulating = true;
+        }
+    }, false);
+
+    // Reset
+    document.getElementById('action-reset').addEventListener('click', event => {
+        population.repopulate(initialCity);
+        simulationState.generationCount = 0;
+    });
+
+    // Sidebar EventListeners
+
+    // Populationsgröße
+    document.getElementById('action-set-populations-size').value = populationConfig.size;
+    document.getElementById('action-set-populations-size').addEventListener('input', event => {
+        let value = Number(event.target.value);
+        if (value > 1) {
+            populationConfig.size = value;
+            population.updateConfig(populationConfig);
+        } else {
+            event.target.value = 2;
+        }
+    });
+
+    // Knotenpunkt-bewege-Chance
+    document.getElementById('action-set-position-mutation-rate').value = populationConfig.moveChance;
+    document.getElementById('action-set-position-mutation-rate').addEventListener('input', event => {
+        let value = Number(event.target.value);
+        if (value <= 1 && value >= 0) {
+            populationConfig.moveChance = value;
+            population.updateConfig(populationConfig);
+        } else {
+            event.target.value = (value > 1) ? 1 : 0;
+        }
+    });
+
+    // Bewegungsmaximum bei Knotenpunkt-Bewegung
+    document.getElementById('action-set-position-mutation-max').value = populationConfig.maxMoveDelta;
+    document.getElementById('action-set-position-mutation-max').addEventListener('input', event => {
+        let value = Number(event.target.value);
+        if (value >= 1) {
+            populationConfig.maxMoveDelta = value;
+            population.updateConfig(populationConfig);
+        } else {
+            value = 1;
+        }
+    });
+
+    // Neuverbindungschance
+    document.getElementById('action-set-reconnect-chance').value = populationConfig.reconnectChance;
+    document.getElementById('action-set-reconnect-chance').addEventListener('input', event => {
+        let value = Number(event.target.value);
+        if (value <= 1 && value >= 0) {
+            populationConfig.reconnectChance = value;
+            population.updateConfig(populationConfig);
+        } else {
+            event.target.value = (value > 1) ? 1 : 0;
+        }
+    });
+
+    // Knotenpunkkosten
+    document.getElementById('action-set-node-penalty').value = populationConfig.nodePenalty;
+    document.getElementById('action-set-node-penalty').addEventListener('input', event => {
+        let value = Number(event.target.value);
+        if (value >= 0) {
+            populationConfig.nodePenalty = value;
+            population.updateConfig(populationConfig);
+        } else {
+            event.target.value = 0;
+        }
+    });
+
+    // Knotenpunktmutationchance
+    document.getElementById('action-set-node-mutation-rate').value = populationConfig.nodeMutationChance;
+    document.getElementById('action-set-node-mutation-rate').addEventListener('input', event => {
+        let value = Number(event.target.value);
+        if (value >= 0) {
+            populationConfig.nodeMutationChance = value;
+            population.updateConfig(populationConfig);
+        } else {
+            event.target.value = 0;
+        }
+    });
+
+    requestAnimationFrame(animate);
+}
+
+document.addEventListener("DOMContentLoaded", sketch);
