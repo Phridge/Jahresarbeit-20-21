e2d01aa0795586090ba80ec00a5ccf937ad018c3
Erdwig <rl.foerster.2002@gmail.com>
2021-02-05 14:46:15 +0100

    added comments to chapter 6 dom

--- code_begleitung_tutorial/06_dom/js/sketch.js
+++ code_begleitung_tutorial/06_dom/js/sketch.js
@@ -1,22 +1,33 @@
-
+// in das Dokument schreiben während die Seite geladen wird
 document.write("Hallo Dokument!");
 
+// den Text-Inhalt des Titels ändern
 let titel = document.getElementById("titel");
 titel.innerText = "Ein schöner Titel";
 
+// DOM-Objekte über "getElementsByTagName" abfragen
 let canvas = document.getElementsByTagName("canvas");
 console.log(canvas);
 canvas = canvas[0];
 console.log(canvas);
 
+// Attribute des Canvas ändern
 console.log(canvas.getAttribute("width"));
 canvas.setAttribute("height", 666);
 console.log(canvas.getAttribute("height"));
 
+// Demonstration von "querySelectorAll"
+// in diesem Fall "alle Elemente mit der id "liste""
 let liste = document.querySelectorAll("#liste")[0];
 
+// neues Element erstellen
 let newElement = document.createElement("li");
+
+// den HTML-Inhalt ändern
 newElement.innerHTML = "Zweites Element <i>sogar mit Tags!</i>";
 
+// das neue Element an ein anderes anfügen
 liste.appendChild(newElement);
+
+// Ein "Test-Element", also rohen Text, anhängen
 newElement.appendChild(document.createTextNode("<h2>Browser austricksen leicht gemacht!</h2>"));
